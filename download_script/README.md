8c6022d6233922817eaafde291b1aaf102651d74
Mike Wittenauer <mikewittenauer@me.com>
2021-12-19 15:06:10 -0500

    basic download script

---
+++ download_script/README.md
@@ -0,0 +1,9 @@
+# Lego Instruction Download Script
+
+## Description
+This project uses ruby library 'watir' to open a browser and pull up the pages to download instructions
+
+## Installation
+
+- gem install 'watir' - Main library for scripting browser interactions
+- gem install 'webdrivers' - Web driver gem  which allows the script to access the local browser
---
+++ download_script/script.rb
@@ -0,0 +1,43 @@
+require 'webdrivers'
+require 'watir'
+
+prefs = {
+  download: {
+    'prompt_for_download' => false,
+    'default_directory' => '/Users/mikewittenauer/Desktop/lego_instructions/download_script/downloads/75300'
+  },
+  plugins: {
+    'always_open_pdf_externally' => true
+  }
+}
+
+browser = Watir::Browser.new :chrome, options: {prefs: prefs}
+
+#browser = Watir::Browser.start 'https://www.lego.com/en-us/service/buildinginstructions/'
+
+browser.goto 'https://www.lego.com/en-us/service/buildinginstructions/'
+
+age_gate = browser.element(class: 'AgeGatestyles__Wrapper-xudtvj-0').wait_until(&:exists?)
+age_gate_button = browser.element(class: ['Button__Base-sc-1jdmsyi-0', 'eLFkVi AgeGatestyles__StyledButton-xudtvj-12', 'hycfPw'])
+age_gate_button.click
+
+cookie_gate = browser.element(class: ['Modalstyles__ModalWindow-l3z049-1', 'ivppUQ']).wait_until(&:exists?)
+cookie_gate_button = browser.element(class: ['Button__Base-sc-1jdmsyi-0 aKFCv CookieModalstyles__PrimaryButton-sc-19wlthm-7', 'fzEDYJ'])
+cookie_gate_button.click
+
+search_box = browser.text_field(id: 'search')
+search_box.set '75300'
+
+search_button = browser.button(class: ['c-form-item__icon', 'c-btn', 'c-btn--silent'])
+search_button.click
+
+instruction_result = browser.element(id: '75300').wait_until(&:exists?)
+instruction_result_link = browser.element(class: ['c-card__img', 'o-inline-img', 'o-inline-img--contain', 'o-inline-img--inset', 'c-btn', 'o-ratio', 'o-ratio--four-three', 'o-ratio--fit-vert'])
+instruction_result_link.click
+
+download_link = browser.element(class: ['c-card', 'c-card--bi-list']).wait_until(&:exists?)
+download_link.click
+
+sleep(10)
+
+browser.close
